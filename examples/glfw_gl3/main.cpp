f5172175b9990692f2665edaf95ac89053a41b6a
Martin Lombardo <to@mlomb.me>
2020-11-30 20:17:26 -0300

    enabled glew

--- examples/glfw_gl3/main.cpp
+++ examples/glfw_gl3/main.cpp
@@ -4,14 +4,14 @@
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #define GLFW_INCLUDE_ES3
+#else
+#define GLFW_INCLUDE_NONE
+#include <gl/glew.h>
 #endif
 
 #include <GLFW/glfw3.h>
 
 #include <flexui/Element.hpp>
-
-const unsigned int width = 320 * 4;
-const unsigned int height = 180 * 4;
 
 GLFWwindow* window = nullptr;
 static bool main_loop_running = false;
@@ -19,6 +19,9 @@ static bool main_loop_running = false;
 void main_loop() {
 	glClearColor(1.0, 0.3, 0.3, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 	
 	{
@@ -30,16 +33,16 @@ void main_loop() {
 			1.0, -1.0,
 			1.0,  1.0
 		};
-		//unsigned int VBO;
-		//glGenBuffers(1, &VBO);
-		//glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		//glBufferData(GL_ARRAY_BUFFER, 12, verts, GL_STATIC_DRAW);
+		unsigned int VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, 12, verts, GL_STATIC_DRAW);
 
 		using namespace flexui;
 		Element* e = new Element();
 		delete e;
 
-		//glDeleteBuffers(1, &VBO);
+		glDeleteBuffers(1, &VBO);
 	}
 
 	glfwSwapBuffers(window);
@@ -57,12 +60,18 @@ int main(int, char**) {
 		throw new std::runtime_error("GLFW failed to initialize");
 	}
 
-	window = glfwCreateWindow(width, height, "flexui demo", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "flexui demo", NULL, NULL);
 	if (!window) {
 		throw new std::runtime_error("GLFW window creation failed");
 	}
 
 	glfwMakeContextCurrent(window);
+
+	#ifndef __EMSCRIPTEN__
+	if (glewInit() != GLEW_OK) {
+		throw new std::runtime_error("GLEW failed to initialize");
+	}
+	#endif
 
 	const GLubyte* renderer = glGetString(GL_RENDERER);
 	const GLubyte* version = glGetString(GL_VERSION);
@@ -74,11 +83,10 @@ int main(int, char**) {
 	#ifdef __EMSCRIPTEN__
 	emscripten_set_main_loop(main_loop, 0, 1);
 	#else
-	while (main_loop_running) {
-		main_loop();
-	}
+	while (main_loop_running) main_loop();
 	#endif
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
 
 	return 0;
